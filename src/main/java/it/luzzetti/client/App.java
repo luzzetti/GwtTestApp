8982f55a8e0883cc4a5e1597cbad62800c03ab70
Christian <christian.luzzetti@gmail.com>
2021-05-31 21:00:21 +0200

    creata la struttura di base del progetto

---
+++ src/main/java/it/luzzetti/client/App.java
@@ -0,0 +1,14 @@
+package it.luzzetti.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Hello world!
+ */
+public class App implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+    }
+
+}
